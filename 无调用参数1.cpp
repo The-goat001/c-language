189fa36f98eedb58dea4cef1a1bbde994f0c43fa
The-goat001 <421391629@qq.com>
2021-01-08 12:19:30 +0800

    0000

---
+++ 无调用参数1.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	void a();
+	void b();
+	void c();
+	a();
+	b();
+	c();
+	c();
+	return 0;
+ } 
+ void a()
+ {
+ 	printf("5848547488\n");
+ }
+ void b()
+ {
+ 	printf("fauyehuhfdjhf\n");
+ }
+ void c()
+ {
+ 	printf("dfsaujiweruf\n");
+ }
