f7c88d94c32d2d5084fdc603b9075a435c90100c
The-goat001 <421391629@qq.com>
2021-03-03 20:23:50 +0800

    0.0

---
+++ 3.2.c
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int facl(int n)
+//{
+//	int i = 0;
+//	int ret = 1;
+//	for (i = 1; i <= n; i++)
+//	{
+//		ret *= i;
+//
+//	}
+//	return ret; 
+//int fib(int n)
+//{
+//	if (n <= 2)
+//		return 2;
+//	else
+//		return fib(n - 1) + fib(n - 2);
+//}
+//int fib(int n)//迭代
+//{
+//	int a = 1;
+//	int b = 1;
+//	int c = 1;
+//	while (n > 2)
+//	{
+//		c = a + b;
+//		a = b;
+//		b = c;
+//		n--;
+//	}
+//	return c;
+//}
+//
+//int main()//斐波那契数列 前两和对于第三
+//{
+//	int ret = 0;
+//	int n = 0;
+//	scanf("%d", &n);
+//	ret = fib(n);
+//	printf("%d\n", ret);
+//
+//	return 0;
+//
+//}
+
