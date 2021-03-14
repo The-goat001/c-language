48bc2dfcb9621e15dc7c8bc6aca79e7f1a27b23d
The-goat001 <421391629@qq.com>
2021-03-14 15:25:16 +0800

    、

---
+++ 3_13.2.c
@@ -0,0 +1,78 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		arr[i] = i;
+//	}
+//	return 0;
+// }
+// 
+//
+//void my_strcpy(char* dest, char* src)
+//{
+//	while(*src!='\0')
+//	{
+//		*dest = *src;
+//		src++;
+//		dest++;
+//	}
+//	*dest = *src;
+//}
+#include<assert.h>
+//char* my_strcpy(char* dest, const char* src)
+//{
+//	char* ret = dest;
+//	//if (dest != NULL && src != NULL)
+//	assert(dest!=NULL);//╤оят
+//	assert(src != NULL);
+//		while (*dest++ = *src++)
+//		{
+//			;
+//		}
+//		return ret;
+//}
+// 
+//
+
+
+//int main()
+//{
+//	char arr1[] = "###########";
+//	char arr2[] = "bit";
+//	my_strcpy(arr1, arr2);
+//	printf("%s\n", arr1);
+//	return 0;
+//}
+
+//int main()
+//{
+//	const int num = 10;
+//	const int* p = &num;
+//	*p = 20;
+//	printf("%d\n", num);
+//	return 0;
+//}
+//int my_strlen(const char* str)
+//{
+//	int count = 0;
+//	assert(str != NULL);
+//	while (*str != '\0')
+//	{
+//		str++;
+//		count++;
+//	}
+//	return count;
+//}
+//int main()
+//{
+//	char arr[] = "abc";
+//	int len = my_strlen(arr);
+//	printf("%d\n", len);
+//	return 0;
+//}
